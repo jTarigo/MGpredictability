e221b075144dec14fb79ff50df43bbe650cfcf3a
Documenter.jl <documenter@juliadocs.github.io>
2022-11-11 00:13:44 +0000

    build based on 9e64bd0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Mackey-Glass-Predictability","page":"Home","title":"Mackey-Glass Predictability","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Modules = [MGpredictability]","category":"page"}]
+}
