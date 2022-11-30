9651cd70533d5e6786e48faa17aedbd533f5f884
jTarigo <105504447+jTarigo@users.noreply.github.com>
2022-11-30 10:58:46 -0300

    Update lyapunovExponent.jl

--- src/Functions/lyapunovExponent.jl
+++ src/Functions/lyapunovExponent.jl
@@ -48,14 +48,12 @@ function MLE(α_lim::Tuple, Γ_lim::Tuple; n = 4::Int64, N = 396::Int64, x_in = 
     Γ_vals = Γ_lim[1] : (Γ_lim[2] - Γ_lim[1]) / (M - 1) : Γ_lim[2]
 
     i = 1
-    for α in α_vals
+    Threads.@threads for α in α_vals
         j = 1
 
         for Γ in Γ_vals
 
-            S = DynSys(MG(α, Γ, n, N), x_in, discreteSolver())
-
-            λ[i, j] = lyapunov(S, S.p.N * 1000)
+            λ[i, j] = lyapunov(DynSys(MG(α, Γ, n, N), x_in, discreteSolver()), N * 1000)
 
             j += 1
         end
